b1f11008e3c169b03acf88d3b6e6cbd2e53b3353
Qkk49 <kapce4333@gmail.com>
2022-12-02 23:41:57 +0400

    2

---
+++ ReadMe/ReadMe/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  ReadMe
+//
+//  Created by Павел Клымко on 02.12.22.
+//
+
+struct Book {
+    let title: String
+    let author: String
+    
+    init(title: String = "Title", author: String = "Author") {
+        self.title = title
+        self.author = author
+    }
+}
