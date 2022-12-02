69d06f24bf21906261c45ff504153f1af5eaf8fe
Qkk49 <kapce4333@gmail.com>
2022-12-02 21:49:37 +0400

    1

---
+++ ReadMe/ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Павел Клымко on 02.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
